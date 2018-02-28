9d70a6a7288b56641cfd50c5d3941fd8fe24a2f3
Donovancm <donovan.dcm@gmail.com>
2018-02-28 13:23:45 +0100

    Done linkedList 1%

--- AlgorithmPracticeDev/Unit 3/LinkedLists.cs
+++ AlgorithmPracticeDev/Unit 3/LinkedLists.cs
@@ -11,7 +11,7 @@ namespace AlgorithmPracticeDev.Unit_3
 
         public static LinkedList<Node> LinkedListInsert(LinkedList<Node> listNode, Node node)
         {
-            Console.WriteLine("Integer Array Input");
+            Console.WriteLine("Node List Input");
             foreach (var item in listNode)
             {
                 Console.WriteLine(item.value);
@@ -22,26 +22,88 @@ namespace AlgorithmPracticeDev.Unit_3
                 return listNode;
             }
             var p = listNode.First;
-            while (p.Next != null && p.Next.Value.value <= node.value )
+            while (p.Next != null && p.Next.Value.value <= node.value)
             {
                 p = p.Next;
             }
-            listNode.AddAfter(p, node);   
-             
-            Console.WriteLine("Integer Array Output");
+            listNode.AddAfter(p, node);
+
+            Console.WriteLine("Node List Output");
             foreach (var item in listNode)
             {
                 Console.WriteLine(item.value);
             }
             return listNode;
         }
-        public static void LinkedListSearch()
+        public static LinkedList<Node> LinkedListSearch(LinkedList<Node> listNode, Node node)
         {
-
+            if (listNode.First != null && listNode.First.Value.value == node.value)
+            {
+                Console.WriteLine("Found node with value: " + node.value +" at the start.");
+                return listNode;
+            }
+            var p = listNode.First;
+            while(p != null && p.Value.value != node.value)
+            {
+                try
+                {
+                    p = p.Next;
+                    if (p.Value.value == node.value)
+                    {
+                        Console.WriteLine("Found node with value: " + node.value);
+                    }
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine("Node with value: " + node.value + " not found");
+                    
+                }
+            }
+            return listNode;
         }
-        public static void LinkedListDelete()
+        public static LinkedList<Node> LinkedListDelete(LinkedList<Node> listNode, Node node)
         {
-
+            Console.WriteLine("Node List Input");
+            foreach (var item in listNode)
+            {
+                Console.WriteLine(item.value);
+            }
+            if (listNode.First == null || listNode.First.Value.value > node.value)
+            {
+                //Niet goed
+                //listNode.First.Value = node;
+                Console.WriteLine("Node with value: " +node.value+ " does not exist to delete.");
+                return listNode;
+            }
+            else
+            {
+                if (listNode.First.Value.value == node.value)
+                {
+                    //goed
+                    Console.WriteLine("Node with value: " + node.value + " has been deleted.");
+                    listNode.Remove(listNode.First);
+                    return listNode;
+                }
+            }
+            var x = listNode.First;
+            try
+            {
+                while (x.Next.Value != null && x.Next.Value.value <= node.value)
+                {
+                    if (x.Next.Value.value == node.value)
+                    {
+                        Console.WriteLine("Node with value:" + node.value + "has been deleted.");
+                        listNode.Remove(x.Next.Value);
+                        return listNode;
+                    }
+                    x = x.Next;
+                }
+            }catch (NullReferenceException)
+            {
+                //Niet goed
+                Console.WriteLine("Node with value: " + node.value + " not found to be deleted.");
+            }
+            return listNode;
         }
         public static void LinkedListPrint()
         {
@@ -51,7 +113,9 @@ namespace AlgorithmPracticeDev.Unit_3
             listNode.AddLast(new Node(44));
             listNode.AddLast(new Node(55));
 
-            LinkedListInsert(listNode, new Node(30));
+            //LinkedListInsert(listNode, new Node(30));
+            //LinkedListSearch(listNode, new Node(45));
+            LinkedListDelete(listNode, new Node(21));
 
         }
     }
