a1c63d303ef77ecb7795ac012da2f5827597c0fc
Donovancm <donovan.dcm@gmail.com>
2018-03-01 11:20:20 +0100

    Doubly Linked List Done

--- AlgorithmPracticeDev/Unit 3/DoublyLinkedLists.cs
+++ AlgorithmPracticeDev/Unit 3/DoublyLinkedLists.cs
@@ -10,13 +10,13 @@ namespace AlgorithmPracticeDev.Unit_3
         public static void InsertAfter(DoublyLinkedList list, DLinkedNode node, DLinkedNode newNode)
         {
             newNode.prev = node;
-            newNode = node.next;
             if (node.next == null)
             {
                 list.lastNode = newNode;
             }
             else
             {
+                newNode.next = node.next;
                 node.next.prev = newNode;
             }
             node.next = newNode;
@@ -52,11 +52,33 @@ namespace AlgorithmPracticeDev.Unit_3
         }
         public static void Insertlast(DoublyLinkedList list, DLinkedNode newNode)
         {
-
+            if (list.lastNode == null)
+            {
+                InsertBeginning(list, newNode);
+            }
+            else
+            {
+                InsertAfter(list, list.lastNode, newNode);
+            }
         }
         public static void RemoveNode(DoublyLinkedList list, DLinkedNode node)
         {
-
+            if (node.prev == null)
+            {
+                list.firstNode = node.next;
+            }
+            else
+            {
+                node.prev.next = node.next;
+            }
+            if (node.next == null)
+            {
+                list.lastNode = node.prev;
+            }
+            else
+            {
+                node.next.prev = node.prev;
+            }
         }
         public static void DoublyLinkedListPrint()
         {
@@ -66,15 +88,24 @@ namespace AlgorithmPracticeDev.Unit_3
             DLinkedNode node2 = new DLinkedNode(33);
             DLinkedNode node3 = new DLinkedNode(44);
             DLinkedNode node4 = new DLinkedNode(55);
+            DLinkedNode node5 = new DLinkedNode(66);
             InsertBeginning(list, node1);
             InsertAfter(list, list.firstNode, node2);
             InsertAfter(list, node2, node3);
             InsertAfter(list, node3, node4);
+            Insertlast(list, node5);
+            RemoveNode(list,node5);
 
             var p = list.firstNode;
-            while (p.next !=null)
+            while (p != null)
             {
                 Console.WriteLine("value: "+ p.data);
+               
+                if (p.next.data == list.lastNode.data)
+                {
+                    Console.WriteLine("value: " + p.next.data);
+                    break;
+                }
                 p = p.next;
             }
 
@@ -82,8 +113,8 @@ namespace AlgorithmPracticeDev.Unit_3
         public class DLinkedNode
         {
             public int data;
-            public DLinkedNode next { get; set; }
-            public DLinkedNode prev { get; set; }
+            public DLinkedNode next = null;
+            public DLinkedNode prev = null;
             public DLinkedNode(int data, DLinkedNode next, DLinkedNode prev)
             {
                 this.data = data;
